27d9db59d8c0533e3c951cf5d544bc61c29bedf3
nothotdot <uni.fin.pro2019@gmail.com>
2019-12-20 02:58:23 +0500

    Updated  Comic Website  Detail  View

--- Comic_website/Controllers/ComicWebsiteController.cs
+++ Comic_website/Controllers/ComicWebsiteController.cs
@@ -10,6 +10,15 @@ namespace Comic_website.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.NewsTitle = "Court Wordicts on Mushaeaf";
+            ViewBag.IssueDate = "19 December 2019";
+            ViewBag.Descryption = "<p>Court wordect</p>";
+            ViewBag.NewsInfo = new string[]
+        {
+              "Reporter : Hamza",
+              "Source : CNN",
+              "Other : Fox"
+        };
             return View(); 
         }
             }
